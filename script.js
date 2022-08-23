3406044429d28fb62c5bb70cf8c4435547b30f56
MVIVN <mvngwerume@gmail.com>
2022-08-23 23:43:44 +1200

    Add some test code to check that certain inputs and functions are working

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // DOM elements
+const previousInput = document.getElementById('previous-input');
+const currentInput = document.getElementById('current-input');
 
 const allClearBtn = document.getElementById('all-clear');
 const deleteBtn = document.getElementById('delete');
@@ -10,23 +12,29 @@ const numbers = document.querySelectorAll('.number');
 
 
 
+// Input variables
+
+
+
 
 // Button event listeners
 for (const number of numbers) {
     number.addEventListener('click', function() {
-        console.log(this.textContent);
+        anotherString += this.textContent;
+        console.log(anotherString);
     });
 }
 
 for (const operation of operations) {
     operation.addEventListener('click', function() {
-        console.log(this.textContent);
+        anotherString += ` ${this.textContent} `;
+        console.log(anotherString);
     });
 }
 
 equals.addEventListener('click', function() {
     console.log(this.textContent);
-})
+});
 
 allClearBtn.addEventListener('click', function() {
     console.log(this.textContent);
@@ -39,3 +47,37 @@ deleteBtn.addEventListener('click', function() {
 historyBtn.addEventListener('click', function() {
     console.log(this.textContent);
 });
+
+
+
+// Operational functions
+const add = function (array) {
+    return array.reduce((total, current) => parseFloat(total) + parseFloat(current));
+};
+
+const subtract = function (array) {
+    return array.reduce((total, current) => parseFloat(total) - parseFloat(current));
+};
+  
+const multiply = function (array) {
+    return array.reduce((total, current) => parseFloat(total) * parseFloat(current));
+};
+
+const divide = function (array) {
+    return array.reduce((total, current) => parseFloat(total) / parseFloat(current))
+}
+
+
+
+
+
+
+// tests
+let testString = "12345 + 67890";
+let testArray = testString.split(" + ");
+console.log(add(testArray));
+console.log(divide(testArray));
+console.log(subtract(testArray));
+console.log(multiply(testArray));
+
+let anotherString = '';
